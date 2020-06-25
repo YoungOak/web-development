179f1e67728d16472eb759ad42fbe4dbb2bff4d1
Sebastián <sebastianportillab@gmail.com>
2020-06-25 14:11:37 -0500

    First Load Up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
