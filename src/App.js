1ca35491a65ce6987d9b8a9ab07847703bd57a64
Sebastián <sebastianportillab@gmail.com>
2020-07-07 16:07:40 -0500

    UPDATE Responsivenes for SOME components of DropChart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import DropChart from "./components/Chart";
-import { covid_data } from "./covid_data";
 
 
 function App() {
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Bar, Pie, Line} from 'react-chartjs-2';
+import {Chart, Bar, Pie, Line} from 'react-chartjs-2';
 import { Dropdown, Button } from 'semantic-ui-react';
 
 class DropChart extends Component{
@@ -28,10 +28,17 @@ class DropChart extends Component{
                 chartData: {
                     labels: this.state.data[this.state.choice].map(d => new Date(d["dateRep"])),
                     datasets: [{
-                        label: 'Casos',
-                        data: this.state.data[this.state.choice].map(d => d["cases"]),
+                        label: 'Casos Suavizados',
+                        data: this.state.data[this.state.choice].map(d => d["smooth_cases"]),
                         borderColor: "#3e95cd",
                         fill: false
+                    },{
+                        label: 'Casos Diarios',
+                        data: this.state.data[this.state.choice].map(d => d["cases"]),
+                        borderColor: "#0195cd",
+                        backgroundColor: "#d1f2eb ",
+                        fill: true,
+                        type: "bar"
                     }]
                 }
             })
@@ -43,10 +50,17 @@ class DropChart extends Component{
         chartData: {
             labels: this.state.data[value].map(d => new Date(d["dateRep"])),
             datasets: [{
-                label: 'Casos',
-                data: this.state.data[value].map(d => d["cases"]),
+                label: 'Casos Suavizados',
+                data: this.state.data[value].map(d => d["smooth_cases"]),
                 borderColor: "#3e95cd",
                 fill: false
+            },{
+                label: 'Casos Diarios',
+                data: this.state.data[value].map(d => d["cases"]),
+                borderColor: "#0195cd",
+                backgroundColor: "#d1f2eb ",
+                fill: true,
+                type: "bar"
             }]
         }
         }
@@ -67,14 +81,21 @@ class DropChart extends Component{
                 />
                 <Line
                     data = {this.state.chartData}
+                    responsive = {true}
+                    maintainAspectRatio = {true}
                     options = {{
+                        responsive: true,
+                        maintainAspectRatio: true,
                         title: {
                             display: true,
                             text: this.state.choice,
-                            fontSize: 25
+                        },
+                        layout: {
+                            padding: 20
                         },
                         legend: {
-                            display: false
+                            display: true,
+                            padding: 10
                         },
                         scales: {
                             xAxes: [{
@@ -85,6 +106,14 @@ class DropChart extends Component{
                             }]
                         },
                     }}
+                    plugins = {[{
+                        resize: function(c, o) {
+                            var chartHeight = c.chart.height;
+                            var size = 15 + (700-chartHeight)*0.05;
+                            c.scales['y-axis-0'].options.ticks.minor.fontSize = size;
+                            c.scales['x-axis-0'].options.ticks.minor.fontSize = size;
+                        }
+                    }]}
                 />    
             </div>
         )
